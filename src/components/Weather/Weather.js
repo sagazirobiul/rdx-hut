593aaa82e8b5a6c41fcde9011991282a9990e458
sagazirobiul <gazisagor00000@gmail.com>
2021-12-03 10:33:54 +0600

    updated weather forecast

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -11,7 +11,7 @@ const Weather = ({getLatAndLng}) => {
         axios(`https://api.openweathermap.org/data/2.5/onecall?lat=${getLatAndLng.latitude}&lon=${getLatAndLng.longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`)
         .then(res => {
             setWeather(res);
-           
+            console.log(res);
         })
     }, [getLatAndLng.latitude, getLatAndLng.longitude])
 
@@ -21,22 +21,23 @@ const Weather = ({getLatAndLng}) => {
     return (
         <div>
             <div className="text-center">
-                <p>{new Date().toDateString()}</p>
-                <h3>{getWeather.data.timezone}</h3>
+                <p className="todaysDate">{new Date().toDateString()}</p>
+                <h3 className="cityName">{getWeather.data.timezone}</h3>
                 <div className="d-flex justify-content-center align-items-center">
                     <img id="image" src={`https://openweathermap.org/img/wn/${getWeather?.data.current.weather[0].icon}@2x.png`} alt="" />
                     <h4>{getWeather?.data.current.temp} &deg;C</h4>
                 </div>
             </div>
             <div>
-                <h6>Next 3 days forecast</h6>
+                <h6 className="nextThreeDay">Next 3 days forecast</h6>
                 {
                     getThreeDays.map((data, index) => {
                         return(
-                            <div>
+                            <div className="d-flex align-items-center justify-content-around px-2 forecast">
                                 <span>{ new Date(new Date().getTime() + (index + 1) * 24 * 60 * 60 * 1000).toDateString()}</span>
-                                <img id="image" src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" />
-                                <span>{data.temp.day} &deg;C </span>
+                                <div>
+                                    <img id="image" src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" /><span className="fw-bold">{data.temp.day} &deg;C </span>
+                                </div>
                                 <span>{data.weather[0].description}</span>
                             </div>
                         )
